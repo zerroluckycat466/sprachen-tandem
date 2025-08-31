d82d7168b1592b6ebbb046c9bc7913db2b119a9a
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-31 23:00:35 +0800

    Refactor MongoDB connection logging for clarity

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/sprachen-tandem';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.get('/', (req, res) => {
   res.send('Willkommen bei Sprachen Tandem!');
