5603b506e82e8c88bcf16e55a91db1a37bed5e56
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-31 22:58:42 +0800

    Update MongoDB connection message to English.

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sprachen-tandem', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB verbunden'))
-  .catch(err => console.error('MongoDB-Verbindungsfehler:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Willkommen bei Sprachen Tandem!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server läuft auf http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
